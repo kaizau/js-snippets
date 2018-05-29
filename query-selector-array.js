4e651130a9598f920cc9b7d234610c3b3316d8be
Kai Zau <k@kaizau.com>
2018-05-29 17:15:02 +0800

    Check prototype instead of Element

--- query-selector-array.js
+++ query-selector-array.js
@@ -1,5 +1,5 @@
 // Extended querySelectorAll for evil convenience
-if (Element && !Element.querySelectorArray) {
+if (!Element.prototype.querySelectorArray) {
   Element.prototype.querySelectorArray = function elementQuerySelectorArray(selector) {
     return Array.prototype.slice.call(this.querySelectorAll(selector));
   };
