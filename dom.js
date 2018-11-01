23ad43a4d318f9966601e21477db180570a3819e
Kai Zau <k@kaizau.com>
2018-11-01 16:41:40 +0800

    Edit snippets

---
+++ dom.js
@@ -0,0 +1,34 @@
+//
+// Dom-ready IIFE
+//
+(function(doc, load) {
+  doc.addEventListener(load, ready);
+  function ready() {
+    doc.removeEventListener(load, ready);
+
+    // Do stuff
+  }
+})(document, 'DOMContentLoaded');
+
+//
+// Delegated event
+//
+function delegateEvent(container, event, selector, callback) {
+  container.addEventListener(event, function(e) {
+    var target = e.target;
+    do {
+      if (target.matches(selector)) {
+        callback(e);
+        return;
+      }
+    }
+    while (target = target.parentNode)
+  });
+}
+
+//
+// Transform querySelectorAll Nodelist to Array
+//
+function toArray(list) {
+  return Array.prototype.slice.call(list);
+}
