f98ce28ee1b1513ebd31afe3a95e597066923b7d
Kai Zau <k@kaizau.com>
2018-11-09 12:29:37 +0800

    Update dom helpers

--- dom.js
+++ dom.js
@@ -1,4 +1,8 @@
 //
+// Some copy-paste patterns for coding "without guns"
+//
+
+//
 // Dom-ready IIFE
 //
 (function(doc, load) {
@@ -11,9 +15,9 @@
 })(document, 'DOMContentLoaded');
 
 //
-// Delegated event
+// Delegated events
 //
-function delegateEvent(container, event, selector, callback) {
+function delegateEvent(container, selector, event, callback) {
   container.addEventListener(event, function(e) {
     var target = e.target;
     do {
@@ -22,7 +26,7 @@ function delegateEvent(container, event, selector, callback) {
         return;
       }
     }
-    while (target = target.parentNode)
+    while (target = target.parentNode);
   });
 }
 
@@ -34,6 +38,38 @@ function toArray(list) {
 }
 
 //
-// Parse Query String
+// Parse Query String (extracted from mithril.js)
 //
-// TODO
+function parseQueryString(string) {
+  if (string === '' || string == null) return {};
+  if (string.charAt(0) === '?') string = string.slice(1);
+  var entries = string.split('&'),
+    data0 = {},
+    counters = {};
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i].split('=');
+    var key5 = decodeURIComponent(entry[0]);
+    var value = entry.length === 2 ? decodeURIComponent(entry[1]) : '';
+    if (value === 'true') value = true;
+    else if (value === 'false') value = false;
+    var levels = key5.split(/\]\[?|\[/);
+    var cursor = data0;
+    if (key5.indexOf('[') > -1) levels.pop();
+    for (var j = 0; j < levels.length; j++) {
+      var level = levels[j],
+        nextLevel = levels[j + 1];
+      var isNumber = nextLevel == '' || !isNaN(parseInt(nextLevel, 10));
+      var isValue = j === levels.length - 1;
+      if (level === '') {
+        var key5 = levels.slice(0, j).join();
+        if (counters[key5] == null) counters[key5] = 0;
+        level = counters[key5]++;
+      }
+      if (cursor[level] == null) {
+        cursor[level] = isValue ? value : isNumber ? [] : {};
+      }
+      cursor = cursor[level];
+    }
+  }
+  return data0;
+}
