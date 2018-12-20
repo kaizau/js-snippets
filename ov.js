df7eb785175bdbefd7bce7d31acf45716f4ead34
Kai Zau <k@kaizau.com>
2018-12-21 07:41:46 +0800

    Update ov.js snippet

--- ov.js
+++ ov.js
@@ -1,11 +1,111 @@
-// Decorate an accessor function with observable methods
-function observablify(fn) {
-  fn.publish = function publish() {
-    fn.listeners.forEach(handler => handler.call(fn, fn.value, fn.previousValue));
+//
+// An observable value
+//
+// const obsValue = ov('old');
+// obsValue.subscribe(logger);
+// obsValue();        // => 'old'
+// obsValue('old');   // => (identical value, skipped)
+// obsValue('new');   // => 'new', 'old'
+//
+// Modifying arrays and objects will not publish, but replacing them will.
+//
+// const obsArray = ov([1, 2, 3]);
+// obsArray.subscribe(logger);
+// obsArray().push(4);  // => (nothing)
+// obsArray([4, 5]);    // => [4, 5], [1, 2, 3]
+//
+// Passing a function caches the result as the value. Any extra arguments will
+// be passed to the function. Any observables called within the function will
+// be subscribed to, and updates to those observables will recompute the value.
+//
+// const foo = ov(1);
+// const bar = ov(2);
+// const computed = ov(arg => { a() + b() + arg }, 3);
+// computed.subscribe(logger);
+// computed();          // => 6
+// foo(2);              // => Logs 7, 6
+//
+export default function ov(value) {
+  function accessor(newValue) {
+    if (!arguments.length) {
+      // If called inside a computed observable value function, track as a
+      // child observable
+      if (ov._computeActive && ov._computeChildren.indexOf(accessor) === -1) {
+        ov._computeChildren.push(accessor);
+      }
+      return accessor.value;
+    } else if (newValue !== accessor.value) {
+      accessor.previousValue = accessor.value;
+
+      if (typeof newValue !== 'function') {
+        accessor.value = newValue;
+        accessor.publish();
+      } else {
+        // First argument is the value function, additional arguments are
+        // passed to the value function
+        const args = [];
+        for (let i = 1; i < arguments.length; i++) {
+          const arg = arguments[i];
+          args.push(arg);
+        }
+        setComputedValue(accessor, newValue, args);
+      }
+    }
+  }
+
+  accessor.previousValue = null;
+  accessor.value = value;
+  accessor.listeners = [];
+
+  return observablify(accessor);
+}
+
+//
+// Mixin for setting computed values
+//
+// Note, currently there's no shortcut to cleanup a computed value.
+ov._computeActive = false;
+ov._computeChildren = [];
+function setComputedValue(fn, valueFn, args) {
+  fn.compute = function compute() {
+    const result = valueFn.apply(fn, args);
+    if (typeof result !== 'undefined') {
+      if (typeof result.then === 'function') {
+        result.then(asyncResult => fn(asyncResult));
+      } else {
+        fn(result);
+      }
+    }
   };
 
-  fn.subscribe = function subscribe(handler) {
+  // Subscribe to child observables
+  ov._computeActive = true;
+  fn.compute();
+  ov._computeActive = false;
+  ov._computeChildren.forEach(child => child.subscribe(fn.compute));
+  ov._computeChildren.length = 0;
+}
+
+//
+// Mixin for observable-ish methods
+//
+// Pass it an accessor function with the following properties:
+// - .value
+// - .previousValue
+// - .listeners
+//
+function observablify(fn) {
+  fn.publish = function publish() {
+    fn.listeners.forEach(handler =>
+      handler.call(fn, fn.value, fn.previousValue)
+    );
+  };
+
+  fn.subscribe = function subscribe(handler, immediate) {
     fn.listeners.push(handler);
+    if (immediate) {
+      handler.call(fn, fn.value, fn.previousValue);
+    }
   };
 
   fn.unsubscribe = function unsubscribe(handler) {
@@ -16,39 +116,4 @@ function observablify(fn) {
   };
 
   return fn;
-}
-
-//
-// An observable value
-//
-// const obsValue = ov('abc');
-// obsValue.subscribe(logger);
-// obsValue();        // => 'abc'
-// obsValue('abc');   // =>
-// obsValue('def');   // => 'def', 'abc'
-//
-// Modifying arrays and objects will not publish, but replacing them will.
-//
-// const obsArray = ov([1, 2, 3]);
-// obsArray.subscribe(logger);
-// obsArray();          // => [1, 2, 3]
-// obsArray().push(4);  // =>
-// obsArray();          // => [1, 2, 3, 4]
-// obsArray([4, 5]);    // => [4, 5], [1, 2, 3, 4]
-//
-export function ov(value) {
-  function accessor(newValue) {
-    if (arguments.length && newValue !== accessor.value) {
-      accessor.previousValue = accessor.value;
-      accessor.value = newValue;
-      accessor.publish();
-    }
-    return accessor.value;
-  }
-
-  accessor.previousValue = null;
-  accessor.value = value;
-  accessor.listeners = [];
-
-  return observablify(accessor);
 }
